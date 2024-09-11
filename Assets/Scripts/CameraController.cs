355b01aaec1ce56c466053bfa534eee057f61443
Markus Heidemann Lorensen <markuslorensen@gmail.com>
2024-09-11 13:17:29 +0200

    Created the character and camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private float horizontalSensitivity = 10.0f;
+    [SerializeField] private float verticalSensitivity = 10.0f;
+    [SerializeField] private Vector2 rotationMinMax = new Vector2(-40, 40);
+    
+    [SerializeField] private GameObject cameraTarget;
+    [SerializeField] private float distanceFromTarget = 6.0f;
+
+    [SerializeField] private float smoothTime = 0.2f;
+    
+    private float rotationX;
+    private float rotationY;
+
+    private Vector3 currentRotation;
+    private Vector3 smoothVelocity = Vector3.zero;
+    
+
+    private Transform thisTransform;
+
+    private void Start() {
+        thisTransform = transform;
+    }
+
+    private void Update() {
+        PositionCamera();
+        RotateCamera();
+    }
+
+
+    private void RotateCamera() {
+        float mouseX = Input.GetAxis("Mouse X") * verticalSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * horizontalSensitivity;
+
+        rotationY += mouseX;
+        rotationX += mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, rotationMinMax.x, rotationMinMax.y);
+
+        Vector3 nextRotation = new (rotationX, rotationY);
+        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref smoothVelocity, smoothTime);
+
+        transform.localEulerAngles = currentRotation;
+    }
+
+    private void PositionCamera() {
+        thisTransform.position = cameraTarget.transform.position - transform.forward * distanceFromTarget;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float movementSpeed = 0.05f;
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float jumpHeight = 5f;
+    
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject raycastOrigin;
+    
+    private Vector3 playerVelocity;
+    private CharacterController charController;
+
+    private const float Gravity = -9.81f;
+    
+
+    // Start is called before the first frame update
+    void Start() {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        MovePlayer();
+    }
+
+
+    private void MovePlayer() {
+        if (GroundCheck() && playerVelocity.y < 0) {
+            playerVelocity.y = 0f;
+        }
+        
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        
+        Vector3 movementDirection = Quaternion.Euler(0, mainCamera.transform.eulerAngles.y ,0) *  new Vector3(horizontalMovement, 0, verticalMovement).normalized;
+
+        charController.Move(movementDirection * (movementSpeed * Time.deltaTime));
+        
+        if (movementDirection != Vector3.zero) {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+        }
+        
+        if (Input.GetButtonDown("Jump") && GroundCheck()) {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * Gravity);
+        }
+        
+        Debug.Log("is Grounded: " + GroundCheck());
+        Debug.Log("Jump pressed: " + Input.GetButtonDown("Jump"));
+        
+        playerVelocity.y += Gravity * Time.deltaTime;
+        charController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private bool GroundCheck() {
+        Transform originTransform = raycastOrigin.transform;
+
+        return Physics.Raycast(originTransform.position, -originTransform.up, 0.05f);
+    }
+}
